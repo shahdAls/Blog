8e0b5e65b5cdffecc78317abeb7ee4181dddb84d
NAZ <112337066+Nas-Z@users.noreply.github.com>
2024-06-27 14:30:39 +0300

    Login Added

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\hash;
+
+class LoginController extends Controller
+{
+    public function loginUser(Request $request){
+
+        try{
+            $validateUser = Validator::make($request->all(),
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            if ($validateUser -> fails()){
+                return response() ->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser ->errors()
+                ], 401);
+            }
+
+            if (!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Incorrect Email or Password'
+                ],401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+        }
+        catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+}
